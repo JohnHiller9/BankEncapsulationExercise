be5591e655c02bc2eb206fa76aa390b9ef52cef8
John Hiller <jdhiller@bellsouth.net>
2021-06-21 12:00:36 -0500

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+        /* Encapsulation - The only way to access the private balance is to 
+         * make a public deposit. */
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+
+
+
+
+
+        /*
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        */
+
+
+    }
+    // This public property has read (get) and write (set) capabilities.
+
+    public BankAccount(double amount)
+        {
+            balance = 0;
+        }
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount(); //Inferred typing
+            // the BankAccount() is a blueprint
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDep = double.Parse(Console.ReadLine()); //double.Parse converts amount to string
+            account.Deposit(amountToDep);
+            Console.WriteLine($"Thank you! Your balance is now {account.GetBalance()}");
+
+            /*
+             * Example - 
+            var account = new BankAccount();
+            account.Balance = 300;
+            Console.WriteLine(account.Balance);
+            */
+        }
+    }
+}
